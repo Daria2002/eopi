5781b8e86cda26fd59d987b4957154ca1f84c4c9
Daria2002 <daria.matkovic@fer.hr>
2021-03-27 10:13:05 +0100

    Add other approaches

--- src/Primitive_Types/ComputingTheParityOfAWord.cpp
+++ src/Primitive_Types/ComputingTheParityOfAWord.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <unordered_map>
+#include <cmath>
 
-int sol1(unsigned long n)
+/**
+ * O(n) time complexity
+ */
+short sol1(unsigned long n)
 {
     short result = 0;
     while (n)
@@ -18,17 +23,57 @@ int sol1(unsigned long n)
     return result;
 }
 
-int sol2(unsigned long n)
+/**
+ * O(k) is time complexity, where k is the number of set bits
+ */
+short sol2(unsigned long n)
 {
-    
+    short result = 0;
+    while (n)
+    {
+        /*
+            r, k = 1
+            0, 1 -> r = 1
+            1, 1 -> r = 0
+        */
+        result ^= 1;
+        n &= (n - 1); // Drops the lowest set bit of n
+    }
+    return result;
 }
 
-int sol3(unsigned long n)
+/**
+ * O(n/l) time complexity
+ * n - number of bits
+ * l - number of bits in a group
+ */
+std::unordered_map<unsigned long, short> preprocess()
 {
-    
+    std::unordered_map<unsigned long, short> parity_map;
+    for(int i = 0; i < 16; i++)
+    {
+        unsigned long word = pow(2, i);
+        parity_map[word] = sol2(word);
+    }
+    return parity_map;
 }
 
-int sol4(unsigned long n)
+/**
+ * Optimal solution for lot of parity computations.
+ * O(n/l) is time complexity, n - number of bits in a word, l - number of bits in per group
+ */ 
+short sol3(unsigned long n, std::unordered_map<unsigned long, short> parity_map)
+{
+    const int group_size = 16; 
+    const int mask = 0xFFFF;
+    unsigned long first_group = (n >> (3 * group_size));
+    unsigned long second_group = ((n >> (2 * group_size)) & mask); 
+    unsigned long third_group = ((n >> group_size) & mask);
+    unsigned long fourth_group = (n & mask);
+    return (parity_map[first_group] ^ parity_map[second_group] ^ parity_map[third_group] ^ parity_map[fourth_group]);
+}
+
+short sol4(unsigned long n)
 {
     
 }
@@ -42,13 +87,14 @@ int sol4(unsigned long n)
  */
 int main()
 {
-    int method;
+    short method;
     std::cout << "Enter a number of approach you want to use:\n";
     std::cin >> method;
     unsigned long n;
     std::cout << "Enter a number for parity calculation:\n";
     std::cin >> n;
-    int parity;
+    short parity;
+    std::unordered_map<unsigned long, short> parity_map = preprocess(); // key - word, val - parity
     switch (method)
     {
     case 1:
@@ -58,7 +104,7 @@ int main()
         parity = sol2(n);
         break;
     case 3:
-        parity = sol3(n);
+        parity = sol3(n, parity_map);
         break;
     default:
         parity = sol4(n);
